60ee44389eebcbaf2398d1674244e25d60d1276c
Drew Mercer <japanexpress@gmail.com>
2018-03-18 14:45:36 -0400

    Polishing the layout and seed data

--- app/controllers.js
+++ app/controllers.js
@@ -3,33 +3,41 @@
 angular
   .module('punchlistApp.controller', [])
   .controller('punchlistController', [
-    "$scope", "$filter",
+    '$scope',
+    '$filter',
     function($scope, $filter) {
       $scope.newTask = '';
 
       $scope.pendingCount = 3;
 
       $scope.taskList = [
-        { description: 'Tighten bracket - upstairs bonus', done: false },
-        { description: 'Fix nail pops - upstairs bonus', done: false },
-        { description: 'Re-grout tile - upstairs bonus', done: false }
+        { description: 'Tighten bracket - upstairs bonus bath', done: false },
+        { description: 'Fix nail pops - upstairs bonus bath', done: false },
+        { description: 'Empty upstairs hall closet - left side', done: true },
+        { description: 'Re-grout tile - upstairs bonus bath', done: false }
       ];
 
       $scope.addPunch = function() {
-          $scope.taskList.push({description: $scope.newTask, done:false});
-          $scope.newTask = "";
+        $scope.taskList.push({ description: $scope.newTask, done: false });
+        $scope.newTask = '';
       };
 
       $scope.deletePunch = function(index) {
         $scope.taskList.splice(index, 1);
       };
 
-      $scope.$watch('taskList', function() {
-        $scope.pendingCount = $filter('filter')($scope.taskList, {done: false}).length;
-      }, true)
+      $scope.$watch(
+        'taskList',
+        function() {
+          $scope.pendingCount = $filter('filter')($scope.taskList, {
+            done: false
+          }).length;
+        },
+        true
+      );
 
       $scope.clearCompleted = function() {
-          $scope.taskList = $filter('filter')($scope.taskList, {done: false});
-      }
+        $scope.taskList = $filter('filter')($scope.taskList, { done: false });
+      };
     }
   ]);
